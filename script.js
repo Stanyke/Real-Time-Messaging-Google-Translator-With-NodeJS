a80e44df28855a046ba1742433caf10ed8458493
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-02-25 21:45:25 -0800

    Updated script.js

--- script.js
+++ script.js
@@ -15,42 +15,51 @@ else
     socket.emit('new_client', username); // Passes username as new_client
     document.title = username + ' - ' + document.title; // Updates webpage's title adding username to it
 
+    
     // When a message is received it's inserted in the page
-    socket.on('message', function(data) {
+    socket.on('message', (data) => 
+    {
         insertMessage(data.username, data.message)
-                })
+    });
 
-                // When a new client connects, the information is displayed
-                socket.on('new_client', function(username) {
-                    $('#chat_zone').prepend('<p><em>' + username + ' has joined the chat!</em></p>');
-                })
+    
+    // When a new client connects, the information is displayed
+    socket.on('new_client', (username) =>
+    {
+        $('#chat_zone').prepend('<p><em>' + username + ' has joined the chat!</em></p>');
+    })
 
-                // When translator is clicked
-                translateLanguage = () =>
-                {
-                    var messageValue = $('#message').val(); // Gets message value
-                    var languageWanted = $('#language').val(); // Gets language code's value
-                    socket.emit('language', languageWanted); // Sends language code
-                    socket.emit('messageToBeTranformed', messageValue); // Sends the message
-                }
+    
+    // When translator is clicked
+    translateLanguage = () =>
+    {
+        const messageValue = $('#message').val(); // Gets message value
+        const languageWanted = $('#language').val(); // Gets language code's value
+        socket.emit('language', languageWanted); // Sends language code
+        socket.emit('messageToBeTranformed', messageValue); // Sends the message
+    }
 
 
-                $('#chat_form').submit(function () {
-                    var message = $('#message').val();
-                    var languageWanted = $('#language').val();
-                    socket.emit('message', message); // Sends the message to the others
-                    insertMessage(username, message); // Also displays the message on our page
-                    $('#message').val('').focus(); // Empties the chat form and puts the focus back on it
-                    return false; // Blocks 'classic' sending of the form
-                });
+    $('#chat_form').submit( () =>
+    {
+        var message = $('#message').val();
+        socket.emit('message', message); // Sends the message to the others
+        insertMessage(username, message); // Also displays the message on our page
+        $('#message').val('').focus(); // Empties the chat form and puts the focus back on it
+        return false; // Blocks 'classic' sending of the form
+    });
                 
-                // Adds a message to the page
-                function insertMessage(username, message) {
-                    $('#chat_zone').prepend('<p><strong>' + username + '</strong> ' + message + '</p>');
-                }
+    
+    // Adds a message to the page
+    insertMessage = (username, message) =>
+    {
+        $('#chat_zone').prepend('<p><strong>' + username + '</strong> ' + message + '</p>');
+    }
 
-                // Gets The Transformed language gotten from the index.js file
-                socket.on('transformedLanguage', function(transformedLanguage){
-                    $('#message').val(transformedLanguage); // Replace current message value with the transformed language
-                });
-            }
+    
+    // Gets The Transformed language gotten from the index.js file
+    socket.on('transformedLanguage', (transformedLanguage) =>
+    {
+        $('#message').val(transformedLanguage); // Replace current message value with the transformed language
+    });
+}
