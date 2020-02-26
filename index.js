de9b2e7935537008e6b527ddd61f3e21c4dee5c8
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-02-25 20:12:14 -0800

    Added Google Translator's API

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const express = require('express'),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
     ent = require('ent'), // Blocks HTML characters (security equivalent to htmlentities in PHP)
     fs = require('fs');
 
+const translate = require('@k3rn31p4nic/google-translate-api');
+
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
 // Loading the page index.html
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
   
-io.sockets.on('connection', function (socket, username) {
+io.sockets.on('connection', function (socket, username)
+{
     // When the username is received it’s stored as a session variable and informs the other people
     socket.on('new_client', function(username){
         username = ent.encode(username);
         socket.username = username;
         socket.broadcast.emit('new_client', username);
     });
+    
+    // When the language and message value is received
+    socket.on('language', function(languageWanted){
+
+        socket.once('messageToBeTranformed', function(messageValue){
+        
+            console.log("Hello msg: "+messageValue);
+            
+            translate(messageValue, { to: languageWanted }).then(res =>
+                {
+                    console.log(res.text);
+
+                    const transformedLanguage = res.text;
+                    socket.emit('transformedLanguage', transformedLanguage);
+                }).catch(err => {
+                    console.log(err);
+                });
+        });
+    });
+
+
+    //Translator Setup
+    /* 
+    translate('Ik spreek Engels', {to: 'en'}).then(res => {
+        console.log(res.text);
+        //=> I speak English
+        console.log(res.from.language.iso);
+        //=> nl
+    }).catch(err => {
+        console.error(err);
+    });
+    */
   
+
     // When a message is received, the client’s username is retrieved and sent to the other people
     socket.on('message', function (message) {
         message = ent.encode(message);
@@ -27,9 +63,10 @@ io.sockets.on('connection', function (socket, username) {
 });
 
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
+const port = process.env.PORT || 8080;
+
+server.listen(port, () =>
 {
     console.log('Server Running On Port '+port);
 })
